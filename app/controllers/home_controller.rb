43a81a134188fc465a915d552cc0f767f7d1f767
zyzone03 <zyzone03@naver.com>
2015-09-30 05:31:53 +0000

    first rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+    def login
+    end
+    
+    def posting
+    end
+    
+    def schedule
+    end
+    
+end
